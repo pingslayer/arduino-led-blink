10eeba7027582a30f018e0797d3df46bf7eb088a
pingslayer <ronaldprajval7@gmail.com>
2017-05-31 18:26:00 +0530

    Add files via upload

---
+++ blinkLed.ino
@@ -0,0 +1,31 @@
+int ledPin = 13;
+int blinkInterval = 100;
+int blinkCountLimit = 1;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  blinkLed();
+  delay(2000);
+
+}
+
+void blinkLed() {
+  Serial.print("blinking started.....");
+  for(int blinkCount = 1; blinkCount <= blinkCountLimit; blinkCount++) {
+    digitalWrite(ledPin, HIGH);
+    delay(blinkInterval); 
+    digitalWrite(ledPin, LOW);
+    delay(blinkInterval); 
+  }
+  Serial.print("blinking stopped");
+  Serial.print(" (");
+  Serial.print(blinkCountLimit);
+  Serial.println(")");
+  blinkCountLimit++;
+}
+
+
